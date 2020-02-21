ff83d8089b48847c5fa8eef49e52f3ebeb9adc38
ponta027 <ponta027@gmail.com>
2020-02-21 13:45:23 +0900

    read binarydata

--- qrsample/app.js
+++ qrsample/app.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+var fs = require('fs');
+var bodyParser = require('body-parser')
+
+require('date-utils');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -17,11 +24,31 @@ var options = {
 var server = https.createServer(options,app);
 
 app.post('/sample', function (req, res) {
-    console.log(req.body);
-    res.writeHead(200);
-    res.end("Hello World.");
+//    console.log(req.body);
+      var dt = new Date();
+      var formatted = dt.toFormat("YYYYMMDDHH24MISS");
+      var dataFileName = formatted + ".mid";
+    for (var i in req.body)
+    {
+        console.log(i)
+        console.log(req.body[i])
+        const decodedFile = new Buffer(req.body[i], 'base64');
+        fs.appendFileSync("public/"+dataFileName, decode.slice(2));
+
+    }
+    res.json({result:true,file:dataFileName});
 });
 
+app.post('/analyzeQR', function (req, res) {
+    console.log(req.body);
+    const decodedFile = new Buffer(req.body[0], 'base64');
+    res.json(
+    { result:true,
+      No: decodeFile[0],
+      totalNo: decodeFile[1]
+    }
+    );
+});
 app.use((req, res) => {
   res.sendStatus(404);
 });
