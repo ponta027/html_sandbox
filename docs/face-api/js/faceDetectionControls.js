3d68da26e99773e594e6d88de112686aed155c73
ponta027 <ponta027@gmail.com>
2020-05-17 01:00:19 +0900

    modify root

--- docs/face-api/js/faceDetectionControls.js
+++ docs/face-api/js/faceDetectionControls.js
@@ -88,7 +88,7 @@ async function changeFaceDetector(detector) {
 
   $('#loader').show()
   if (!isFaceDetectionModelLoaded()) {
-    await getCurrentFaceDetectionNet().load('/')
+    await getCurrentFaceDetectionNet().load('/face-api')
   }
 
   $(`#${detector}_controls`).show()
